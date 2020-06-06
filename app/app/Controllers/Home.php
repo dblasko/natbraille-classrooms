4487b68f806e2cca5a55d2ebfa166b6d6c74888e
dblasko <daniel.blasko.dev@gmail.com>
2020-06-06 19:07:30 +0200

    Integrated Twig to the project and added a usage example

--- app/app/Controllers/Home.php
+++ app/app/Controllers/Home.php
@@ -4,7 +4,9 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		//return view('welcome_message');
+        $twig = twig_instance();
+        $twig->display ('layout.html', ['user_first_name' => 'test']);
 	}
 
 	//--------------------------------------------------------------------
