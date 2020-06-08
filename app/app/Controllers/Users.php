5712dacf398e32c9b2957a6ba77b47cd36167eed
dblasko <daniel.blasko.dev@gmail.com>
2020-06-08 18:29:00 +0200

    Added mail validation + implemented login functionallity

--- app/app/Controllers/Users.php
+++ app/app/Controllers/Users.php
@@ -1,6 +1,7 @@
 <?php namespace App\Controllers;
 
 use App\Classes\UserEntity as UserEntity;
+use App\Models\UsersModel;
 
 class Users extends BaseController {
 
@@ -26,8 +27,11 @@ class Users extends BaseController {
         } else if ($userToInsert->getPwd() != hash('sha512', $pwdConfirm)) {
             $invalid_form_input = true;
             $msg = 'Le mot de passe de confirmation ne correspond pas à celui renseigné !';
+        } else if (!filter_var($userToInsert->getMail(), FILTER_VALIDATE_EMAIL)) {
+            $invalid_form_input = true;
+            $msg = 'L\'adresse mail renseignée n\'est pas valide.';
         } else {
-            $model = new \App\Models\UsersModel();
+            $model = new UsersModel();
 
             if ($model->getUser($userToInsert->getMail()) != null) { // mail déjà en BD
                 $invalid_form_input = true;
@@ -46,6 +50,39 @@ class Users extends BaseController {
             'invalid_form_input' => $invalid_form_input,
             'title' => isset($title)? $title : null,
             'msg' => isset($msg)? $msg : '',
+        ]);
+    }
+
+
+    public function logIn() {
+        $mail = $this->request->getVar('loginMail');
+        $pwd = $this->request->getVar('loginPwd');
+
+        if (empty($mail) || empty($pwd)) {
+            $invalid_form_input = true;
+            $msg = "Veuillez renseigner votre mail et votre mot de passe afin de vous connecter.";
+        } else {
+            $model = new UsersModel();
+            $userDbData = $model->getUser($mail);
+
+            if ($userDbData == null) {
+                $invalid_form_input = true;
+                $msg = "Il n'existe pas de compte associé à l'adresse mail renseignée.";
+            } else if ($userDbData['pwd'] !== hash('sha512', $pwd)) {
+                $invalid_form_input = true;
+                $msg = "Le mot de passe renseigné est invalide.";
+            } else { // connecté
+                $invalid_form_input = false;
+                // TODO : variables de session etc° ici
+            }
+        }
+
+        $redirect = 'home/'. (($invalid_form_input)? 'logged_out_content.html' : 'workspace.html.twig');
+        $twig = twig_instance();
+        $twig->display($redirect, [
+            'sender_form' => 'de connexion',
+            'invalid_form_input' => $invalid_form_input,
+            'msg' => isset($msg)? $msg : null,
         ]);
     }
 }
