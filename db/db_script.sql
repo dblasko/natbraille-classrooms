2865363a0286639ad7c5aa0168d026bb674e21e5
dblasko <daniel.blasko.dev@gmail.com>
2020-06-10 15:02:54 +0200

    Added ON DELETE CASCADE constraints on DB

--- db/db_script.sql
+++ db/db_script.sql
@@ -14,7 +14,7 @@ CREATE TABLE Notifications (
     isSeen BOOLEAN DEFAULT FALSE,
     userMail VARCHAR(50),
     link VARCHAR(128),
-    CONSTRAINT fk_notifications_users FOREIGN KEY (userMail) REFERENCES Users(mail)
+    CONSTRAINT fk_notifications_users FOREIGN KEY (userMail) REFERENCES Users(mail) ON DELETE CASCADE
 );
 
 CREATE TABLE Promotions (
@@ -41,8 +41,8 @@ CREATE TABLE Invites (
     invitedPersonMail VARCHAR(50),
     inviteIsoDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk_invites PRIMARY KEY (promotionId, invitedPersonMail),
-    CONSTRAINT fk_invites_promotionid FOREIGN KEY (promotionId) REFERENCES Promotions(id),
-    CONSTRAINT fk_invites_invitedpersonmail FOREIGN KEY (invitedPersonMail) REFERENCES InvitedPerson(mail)
+    CONSTRAINT fk_invites_promotionid FOREIGN KEY (promotionId) REFERENCES Promotions(id) ON DELETE CASCADE,
+    CONSTRAINT fk_invites_invitedpersonmail FOREIGN KEY (invitedPersonMail) REFERENCES InvitedPerson(mail) ON DELETE CASCADE
 );
 
 -- Table d'association entre user & promotions -> être membre d'une promo avec un rôle
@@ -52,8 +52,8 @@ CREATE TABLE PromotionMemberships (
     joinedPromotionIsoDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     role VARCHAR(25) NOT NULL,
     CONSTRAINT pk_promotionmemberships PRIMARY KEY (promotionId, memberUserMail),
-    CONSTRAINT fk_promotionmemberships_promotionid FOREIGN KEY (promotionId) REFERENCES Promotions(id),
-    CONSTRAINT fk_promotionmemberships_memberusermail FOREIGN KEY (memberUserMail) REFERENCES Users(mail),
+    CONSTRAINT fk_promotionmemberships_promotionid FOREIGN KEY (promotionId) REFERENCES Promotions(id) ON DELETE CASCADE,
+    CONSTRAINT fk_promotionmemberships_memberusermail FOREIGN KEY (memberUserMail) REFERENCES Users(mail) ON DELETE CASCADE,
     CONSTRAINT fk_promotionmemberships_role FOREIGN KEY (role) REFERENCES Roles(roleName)
 );
 
@@ -68,7 +68,7 @@ CREATE TABLE Exercises (
     createdByMail VARCHAR(50),
     idTranscriptionType INT(6),
     CONSTRAINT fk_exercise_idtranscriptiontype FOREIGN KEY (idTranscriptionType) REFERENCES TRANSCRIPTIONTYPE(id),
-    CONSTRAINT fk_exercise_createdbymail FOREIGN KEY (createdByMail) REFERENCES Users(mail)
+    CONSTRAINT fk_exercise_createdbymail FOREIGN KEY (createdByMail) REFERENCES Users(mail) ON DELETE CASCADE
 );
 
 
@@ -89,8 +89,8 @@ create table Attempt
    userMail 				varchar(50),
    score      INT(6),
    attemptTxt VARCHAR(5000),
-   CONSTRAINT fk_id FOREIGN KEY (idAffectation) REFERENCES EXERCISEAFFECTATION(id),
-   CONSTRAINT fk_name FOREIGN KEY (userMail) REFERENCES Users(mail)
+   CONSTRAINT fk_id FOREIGN KEY (idAffectation) REFERENCES EXERCISEAFFECTATION(id) ON DELETE CASCADE,
+   CONSTRAINT fk_name FOREIGN KEY (userMail) REFERENCES Users(mail) ON DELETE CASCADE
 );
 
 create table Error
@@ -101,7 +101,7 @@ create table Error
    idErrorType			int(6),
    idAttempt			int(6),
    CONSTRAINT fk_idError FOREIGN KEY (id) REFERENCES ERRORTYPE(id),
-   CONSTRAINT fk_idAttempt FOREIGN KEY (idAttempt) REFERENCES Attempt(id)
+   CONSTRAINT fk_idAttempt FOREIGN KEY (idAttempt) REFERENCES Attempt(id) ON DELETE CASCADE
 );
 
 create table ExerciseAffectation
@@ -110,8 +110,8 @@ create table ExerciseAffectation
    affectationIsoDate   timestamp,
    idExo                int(6),
    idPromotion  		int(6),
-   CONSTRAINT fk_idExercise FOREIGN KEY (id) REFERENCES Exercises(id),
-   CONSTRAINT fk_idPromotion FOREIGN KEY (id) REFERENCES Promotions(id)
+   CONSTRAINT fk_idExercise FOREIGN KEY (idExo) REFERENCES Exercises(id) ON DELETE CASCADE,
+   CONSTRAINT fk_idPromotion FOREIGN KEY (idPromotion) REFERENCES Promotions(id) ON DELETE CASCADE
 );
 
 create table TranscriptionType
